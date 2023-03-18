af2db71321de9520314036db133453fb7faaf0ee
mohamed <m635379@gmail.com>
2023-03-18 19:45:06 +0200

    add plan text animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+const desc = [
+  "في هذه المرحلة نسعى لاستقطاب الاسر المنتجة ذو الكفاءة و الرغبة للانتقال الى مشاريع صغيرة مستدامة",
+  "في هذه المرحلة يتم قبول المشاركين بناء علي الضوابط والشروط المذكورة",
+  "في هذه المرحلة يتم تقديم برامج تدريبية وتأهيلية للمشاركين بموجز 7 حقائب تطويريه لتمنية المهارات المعرفيه لدي المشاركين",
+  "في هذه المرحبة يتم تقديم تدريب الكتروني تفاعلي لكيفية اعداد دراسة الجدوي للمشاركين",
+  "في هذه المرحلة يتم استقطاب متخصصين لمساندة المشاريع وتمكينها من تحقيق النمو والتوسع وزيادة مبيعات المشاركين السنوية ومتابعتها",
+  "في هذه المرحلة يتم تقديم منتج تمويلي بالاضافة الي متابعة كافة الاعمال بحيث توجهيهم بكيفية الصرف",
+  "في هذه المرحلة يتم تمكين وتطوير مشاريع المشاركين من خلال مختصين في تطوير الاعمال وتنمية المبيعات لدي المشاركين",
+  "في هذه المرحلة يتم دعم مشاريع المشاركين من خلال ربطهم بجهات تصدير وربطهم بمنافذ بيع دولية وتصدير مشاريعهم كقصص نجاح لبرنامج تجسير",
+];
 const plans = document.querySelectorAll(".plans li"),
-  spans = document.querySelectorAll(".plans span");
+  spans = document.querySelectorAll(".plans span"),
+  text = document.querySelector(".plan-text .text"),
+  counter = document.querySelector(".plan-text .odometer");
 let times = 0;
 
 setInterval(() => {
@@ -14,5 +26,12 @@ setInterval(() => {
   });
   plans[times].classList.add("plan");
   spans[times].classList.add("done");
+  text.style.opacity = "0";
+  setTimeout(() => {
+    text.style.opacity = "1";
+    text.innerHTML = desc[times - 1];
+  });
+  console.log();
+  counter.innerHTML = (times + 1).toString().padStart(2, 0);
   times++;
 }, 5000);
